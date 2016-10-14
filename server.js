e8dec4bee6853b9c875baaebf8b80d8b4997415e
Namanyadav123 <namanyadav0007@gmail.com>
2016-10-15 00:29:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/article-two',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/FIRSTPROJECT',function(req,res) {
+app.get('/FIRST PROJECT',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'FIRST PROJECT.html'));
 });
 
