1c1f6b0dca2e3ffd44e30ad19f512b69ac7a7372
Namanyadav123 <namanyadav0007@gmail.com>
2016-10-15 00:26:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,44 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var artilcleone = {
-    title:'article-one',
-    heading:'lets start our web app',
-    date:'13 oct,2016',
-    content: '<p>hello,friends my name is NAMAN YADAV</p>',
-    };
-    function createtemplate (data) {
-        var title=data.title;
-        var heading=data.heading;
-        var content=data.content;
-        var date=data.date;
-    var htmltemplate =
-         '<html>'
-         <head> 
-          '<title>'
-         '<center>${title}</center>'
-         '</title>'
-        '<link href="/ui/style.css" rel="stylesheet" />'
-       '</head>'
-       ' <div>'
-       '${date}'
-       ' </div>'
-    <div>
-                    '<a href = "artilcle-one.html">home</a>'
-               '</div>'
-               ' <div class="naman">'
-                <h1>
-                 ' <B><U>  ${heading}</B></U>'
-                <p>
-                  ' <center>${content}</center>'
-               ' </h1>'
-                '</div>'
-                '</body>'
-'</html>';
-return htmltemplate;
-}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/artilcle-one',function(req,res) {
@@ -53,8 +18,8 @@ app.get('/article-two',function(req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-three',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/FIRST PROJECT',function(req,res) {
+  res.sendFile(path.join(__dirname, 'ui', 'FIRST PROJECT.html'));
 });
 
 app.get('/ui/style.css',function(req,res){
